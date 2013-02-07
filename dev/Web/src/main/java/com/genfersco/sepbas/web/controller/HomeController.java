3eefe664130de5f9e715745540db203f99ce4fe7
Facundo Scoccia <fanky10@gmail.com>
2013-02-07 12:31:32 -0300

    compare result test deletition

--- dev/Web/src/main/java/com/genfersco/sepbas/web/controller/HomeController.java
+++ dev/Web/src/main/java/com/genfersco/sepbas/web/controller/HomeController.java
@@ -4,11 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.genfersco.sepbas.web.constants.WebAppConstants;
 
@@ -22,20 +18,6 @@ public class HomeController extends BaseController {
 	public String home(Model map) {
 		map.addAttribute("mensaje", getHomeMessage());
 		return WebAppConstants.HOMEPAGE;
-	}
-
-	@RequestMapping(value = "/compare", method = RequestMethod.GET)
-	public ModelAndView compare(@RequestParam("input1") String input1,
-			@RequestParam("input2") String input2, ModelMap model) {
-
-		int result = (input1.compareTo(input2));
-		String inEnglish = (result < 0) ? "less than"
-				: (result > 0 ? "greater than" : "equal to");
-		String output = "According to our Comparator, '" + input1 + "' is "
-				+ inEnglish + "'" + input2 + "'";
-		model.addAttribute("output", output);
-		System.out.println("output: " + output);
-		return new ModelAndView("web/compareResult", model);
 	}
 
 	public String getHomeMessage() {
