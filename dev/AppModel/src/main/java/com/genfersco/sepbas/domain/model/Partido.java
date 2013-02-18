1a8bd4e91f780f8e440cfcdc0e8f9398e1fa9859
Facundo Scoccia <fanky10@gmail.com>
2013-02-18 07:32:48 -0300

    repositorio de partidos + test

--- dev/AppModel/src/main/java/com/genfersco/sepbas/domain/model/Partido.java
+++ dev/AppModel/src/main/java/com/genfersco/sepbas/domain/model/Partido.java
@@ -1,5 +1,62 @@
 package com.genfersco.sepbas.domain.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "partidos")
 public class Partido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "partido_id", nullable = false)
+	private Integer id;
+
+	@Column(name = "partido_fecha", nullable = false, unique = true)
+	private Date fecha;
+
+	@Column(name = "partido_resultado_local", nullable = false)
+	private Integer resultadoLocal;
+
+	@Column(name = "partido_resultado_visitante", nullable = false)
+	private Integer resultadoVisitante;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Integer getResultadoLocal() {
+		return resultadoLocal;
+	}
+
+	public void setResultadoLocal(Integer resultadoLocal) {
+		this.resultadoLocal = resultadoLocal;
+	}
+
+	public Integer getResultadoVisitante() {
+		return resultadoVisitante;
+	}
+
+	public void setResultadoVisitante(Integer resultadoVisitante) {
+		this.resultadoVisitante = resultadoVisitante;
+	}
 
 }
---
+++ dev/AppModel/src/main/java/com/genfersco/sepbas/domain/repository/PartidoRepository.java
@@ -0,0 +1,9 @@
+package com.genfersco.sepbas.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.genfersco.sepbas.domain.model.Partido;
+
+public interface PartidoRepository extends JpaRepository<Partido, Integer>{
+
+}
