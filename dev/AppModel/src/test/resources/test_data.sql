45419bfb0071ab2dedf6e9bad2d48d332c975735
Facundo Scoccia <fanky10@gmail.com>
2013-07-12 16:57:50 -0300

    test-data

---
+++ dev/AppModel/src/test/resources/test_data.sql
@@ -0,0 +1,34 @@
+--
+-- Dumping data for table `arbitros`
+--
+
+INSERT INTO `arbitros` (`arbitro_id`, `arbitro_nombre`, `arbitro_apellido`) VALUES (4,'Leonardo',' Zalazar');
+
+--
+-- Dumping data for table `clubes`
+--
+
+INSERT INTO `clubes` (`club_id`, `club_nombre`) VALUES (61,'Un club'),(62,'otro club'),(63,'Libertad'),(64,'Fisherton'),(65,'El Tala'),(66,'Estudiantil');
+
+--
+-- Dumping data for table `cuartos`
+--
+
+INSERT INTO `cuartos` (`cuarto_id`, `cuarto_numero`, `cuarto_partido_id`) VALUES (11,1,21),(12,1,27);
+
+--
+-- Dumping data for table `eventos`
+--
+
+INSERT INTO `eventos` (`evento_id`, `evento_generador_id`, `evento_fec_hora`, `evento_tipo_evento`, `evento_estado`, `evento_cuarto_id`, `evento_jugador_id`) VALUES (10,NULL,'2013-03-04 18:12:14',0,1,11,61),(11,NULL,'2013-03-04 18:12:14',0,1,11,61),(12,NULL,'2013-05-09 01:02:39',0,1,12,61),(13,NULL,'2013-05-09 01:02:39',0,1,12,80),(14,NULL,'2013-05-09 01:02:39',0,1,12,60);
+
+--
+-- Dumping data for table `jugadores`
+--
+
+INSERT INTO `jugadores` (`jugador_id`, `jugador_fec_nac`, `jugador_nombre`, `jugador_apellido`, `jugador_numero`, `jugador_actual_club_id`, `jugador_estado`) VALUES (60,'2013-03-15 03:00:00','Facundo','Scoccia',99,62,1),(61,'2013-03-27 03:00:00','lalal','jajaja',88,61,0),(80,'2013-03-20 03:00:00','lalalaal','jajajaj',11,61,0),(81,'2013-03-14 03:00:00','Marcos','Lewis',22,61,0);
+
+--
+-- Dumping data for table `partidos`
+--
+INSERT INTO `partidos` (`partido_id`, `partido_fecha`, `partido_visitante_club_id`, `partido_local_club_id`, `partido_resultado_local`, `partido_resultado_visitante`) VALUES (16,'2013-03-04',62,61,NULL,NULL),(17,'2013-03-04',62,61,NULL,NULL),(18,'2013-03-04',62,61,NULL,NULL),(19,'2013-03-04',62,61,NULL,NULL),(20,'2013-03-04',62,61,NULL,NULL),(21,'2013-03-04',62,61,NULL,NULL),(26,'2013-03-04',62,61,NULL,NULL),(27,'2013-05-08',62,61,NULL,NULL);
