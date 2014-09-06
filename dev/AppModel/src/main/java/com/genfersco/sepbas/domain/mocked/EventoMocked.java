17e9ae6412e0194f0a414a130083eac631876074
Facundo Scoccia <fanky10@gmail.com>
2014-09-06 15:31:14 -0300

    evento repository test fix

--- dev/AppModel/src/main/java/com/genfersco/sepbas/domain/mocked/EventoMocked.java
+++ dev/AppModel/src/main/java/com/genfersco/sepbas/domain/mocked/EventoMocked.java
@@ -2,33 +2,42 @@ package com.genfersco.sepbas.domain.mocked;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.genfersco.sepbas.domain.model.Cuarto;
 import com.genfersco.sepbas.domain.model.EstadoEvento;
 import com.genfersco.sepbas.domain.model.Evento;
 import com.genfersco.sepbas.domain.model.Jugador;
 import com.genfersco.sepbas.domain.model.TipoEvento;
 import com.genfersco.sepbas.domain.repository.EventoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Component
 public class EventoMocked {
-    @Autowired
-    private EventoRepository eventoRepository;
-    @Autowired
-    private CuartoMocked cuartoMocked;
-    @Autowired
-    private JugadorMocked jugadorMocked;
+	@Autowired
+	private EventoRepository eventoRepository;
+	@Autowired
+	private CuartoMocked cuartoMocked;
+	@Autowired
+	private JugadorMocked jugadorMocked;
+
 	public Evento getEvento() {
-            Cuarto cuarto = cuartoMocked.getCuarto();
-            Jugador jugador = jugadorMocked.getJugador();
+		return getEvento(null);
+	}
+
+	public Evento getEvento(Evento eventoGenerador) {
+		Cuarto cuarto = cuartoMocked.getCuarto();
+		Jugador jugador = jugadorMocked.getJugador();
 		Evento evento = new Evento();
 		evento.setCuarto(cuarto);
 		evento.setFechaHora(new Date(System.currentTimeMillis()));
 		evento.setEstado(EstadoEvento.NO_ELIMINADO);
 		evento.setTipoEvento(TipoEvento.INGRESA_JUGADOR);
 		evento.setJugador(jugador);
-                evento = eventoRepository.save(evento);
+		if (eventoGenerador != null) {
+			evento.setEventoGenerador(eventoGenerador);
+		}
+		evento = eventoRepository.save(evento);
 		return evento;
 	}
 }
--- dev/AppModel/src/test/java/com/genfersco/sepbas/domain/repository/EventoRepositoryTest.java
+++ dev/AppModel/src/test/java/com/genfersco/sepbas/domain/repository/EventoRepositoryTest.java
@@ -2,8 +2,6 @@ package com.genfersco.sepbas.domain.repository;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.Date;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,18 +10,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.genfersco.sepbas.domain.mocked.ClubMocked;
-import com.genfersco.sepbas.domain.mocked.CuartoMocked;
 import com.genfersco.sepbas.domain.mocked.EventoMocked;
-import com.genfersco.sepbas.domain.mocked.JugadorMocked;
-import com.genfersco.sepbas.domain.mocked.PartidoMocked;
-import com.genfersco.sepbas.domain.model.Club;
-import com.genfersco.sepbas.domain.model.Cuarto;
-import com.genfersco.sepbas.domain.model.EstadoEvento;
 import com.genfersco.sepbas.domain.model.Evento;
-import com.genfersco.sepbas.domain.model.Jugador;
-import com.genfersco.sepbas.domain.model.Partido;
-import com.genfersco.sepbas.domain.model.TipoEvento;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/dataAccessContext.xml" })
@@ -32,30 +20,13 @@ public class EventoRepositoryTest {
 	@Autowired
 	private EventoRepository eventoRepository;
 	@Autowired
-        private EventoMocked eventoMocked;
-        @Autowired
-        private CuartoMocked cuartoMocked;
-        @Autowired
-        private JugadorMocked jugadorMocked;
-        
-	private Cuarto cuarto = new Cuarto();
-	private Jugador jugador = new Jugador();
+	private EventoMocked eventoMocked;
+
 	private Evento evento = new Evento();
-	
-	
+
 	@Before
 	public void buildData() {
-                cuarto = cuartoMocked.getCuarto();
-                jugador = jugadorMocked.getJugador();
-                
-                evento = new Evento();
-		evento.setCuarto(cuarto);
-		evento.setJugador(jugador);
-		evento.setFechaHora(new Date(System.currentTimeMillis()));
-		evento.setEstado(EstadoEvento.NO_ELIMINADO);
-		evento.setTipoEvento(TipoEvento.INGRESA_JUGADOR);
-		
-		evento = eventoRepository.save(evento);
+		evento = eventoMocked.getEvento();
 	}
 
 	@Test
@@ -67,16 +38,9 @@ public class EventoRepositoryTest {
 	@Test
 	public void testGuardarEventoHijo() {
 		// evento hijo
-		Evento eventoHijo = new Evento();
-		eventoHijo.setCuarto(cuarto);
-		eventoHijo.setJugador(jugador);
-		eventoHijo.setFechaHora(new Date(System.currentTimeMillis()));
-		eventoHijo.setEstado(EstadoEvento.NO_ELIMINADO);
-		eventoHijo.setTipoEvento(TipoEvento.INGRESA_JUGADOR);
-		eventoHijo.setEventoGenerador(evento);
-		
-		eventoHijo = eventoRepository.save(eventoHijo);
-		assertTrue(eventoHijo!=null && eventoRepository.exists(eventoHijo.getId()));
+		Evento eventoHijo = eventoMocked.getEvento(evento);
+		assertTrue(eventoHijo != null
+				&& eventoRepository.exists(eventoHijo.getId()));
 	}
 
 }
