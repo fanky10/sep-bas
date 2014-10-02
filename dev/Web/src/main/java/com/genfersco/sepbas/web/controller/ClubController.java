bbc723e3ade101c329d810fe9f048f3af079c9ee
fgenga <fgenga@hotmail.com>
2014-10-02 19:55:28 -0300

    club controller edit + delete fix

--- dev/Web/src/main/java/com/genfersco/sepbas/web/controller/ClubController.java
+++ dev/Web/src/main/java/com/genfersco/sepbas/web/controller/ClubController.java
@@ -8,17 +8,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.genfersco.sepbas.app.services.ServicesManager;
 import com.genfersco.sepbas.domain.model.Club;
 import com.genfersco.sepbas.web.constants.WebAppConstants;
 import com.genfersco.sepbas.web.form.ClubForm;
-import com.genfersco.sepbas.web.json.DefaultJSONResponse;
-import com.genfersco.sepbas.web.json.JSONResponse;
 import java.util.List;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
@@ -47,16 +42,18 @@ public class ClubController extends BaseController {
 	}
         @RequestMapping(value = "/clubes/edit/{clubId}", method = RequestMethod.GET)
 	public String editarClub(@PathVariable("clubId") String id, ModelMap map) {
-		// shows view
-                Club club = servicesManager.getClub(id);
-                ClubForm clubForm = new ClubForm();
+            
+            ClubForm clubForm = new ClubForm();
+            Club club = servicesManager.getClub(id);
+            if(club!=null){
                 clubForm.setId(club.getId());
                 clubForm.setNombre(club.getNombre());
                 clubForm.setLocalidad(club.getLocalidad());
-		map.addAttribute("clubForm", clubForm);
-		return WebAppConstants.AGREGAR_ClUB;
-	}
-	@RequestMapping(value = "/clubes/list", method = RequestMethod.GET)
+            }
+            map.addAttribute("clubForm", clubForm);
+            return WebAppConstants.AGREGAR_CLUB;
+        }
+        @RequestMapping(value = "/clubes/list", method = RequestMethod.GET)
 	public String getClubes(Model map) {
 		List<Club> clubes = servicesManager.getClubes();
 		map.addAttribute("clubes", clubes);
@@ -69,23 +66,5 @@ public class ClubController extends BaseController {
             return "redirect:/clubes/list";
 	}
         
-	@RequestMapping(value = "/clubes/delete.json", method = RequestMethod.POST)
-	public @ResponseBody
-	JSONResponse jsonDeleteClub(@RequestParam("id") String id, Model map) {
-		JSONResponse response = null;
-		try {
-			if (StringUtils.hasText(id)) {
-				Integer iId = Integer.parseInt(id);
-				servicesManager.deleteClub(iId);
-				response = new DefaultJSONResponse("OK", "Club eliminado");
-			} else {
-				response = new DefaultJSONResponse("ERROR", "Id club vacio");
-			}
-		} catch (NumberFormatException nfe) {
-			response = new DefaultJSONResponse("ERROR",
-					"Id jugador no es un entero");
-		}
-		return response;
-	}
 
 }
