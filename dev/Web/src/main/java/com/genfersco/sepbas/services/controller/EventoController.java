c9835eff6f81c4c5362d35d40660b2578a64214e
Facundo Scoccia <fanky10@gmail.com>
2014-12-22 09:18:12 -0300

    mejorado el servicio de evt lanzamiento

--- dev/Web/src/main/java/com/genfersco/sepbas/services/controller/EventoController.java
+++ dev/Web/src/main/java/com/genfersco/sepbas/services/controller/EventoController.java
@@ -1,5 +1,12 @@
 package com.genfersco.sepbas.services.controller;
 
+import com.genfersco.sepbas.app.services.EventoManager;
+import com.genfersco.sepbas.app.services.ServicesManager;
+import com.genfersco.sepbas.domain.model.EstadoEvento;
+import com.genfersco.sepbas.domain.model.Evento;
+import com.genfersco.sepbas.domain.model.Jugador;
+import com.genfersco.sepbas.domain.model.TipoEvento;
+import com.genfersco.sepbas.web.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,25 +14,51 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.genfersco.sepbas.web.form.EventoLanzamientoData;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
-public class EventoController {
-	@RequestMapping(value = "/secure/evento/lanzamiento.json",method = RequestMethod.POST)
-	public @ResponseBody ResponseMessage eventoLanzamientoJSON(@RequestBody EventoLanzamientoData evtLanzamientoData){
-		String code = "-1";
-		String mensaje="numero de puntos desconocido";
-		int nroPuntos = evtLanzamientoData.getNumeroPuntos();
-		if(nroPuntos==2 || nroPuntos == 3){
-			code = "0";
-			mensaje="numero de puntos:"+nroPuntos;
-		}
-		ResponseMessage responseMessage = new ResponseMessage();
-		responseMessage.setMessage(mensaje);
-		responseMessage.setCode(code);
-		return responseMessage;
-	}
-	@RequestMapping(value = "/secure/evento/lanzamiento.json",method = RequestMethod.GET)
-	public @ResponseBody ResponseMessage eventoLanzamiento(){
-		return new ResponseMessage();
-	}
+@RequestMapping(value = "/api/secure/evento")
+public class EventoController extends BaseController {
+
+    @Autowired
+    private ServicesManager servicesManager;
+    @Autowired
+    private EventoManager eventoManager;
+
+    @RequestMapping(value = "/lanzamiento.json", method = RequestMethod.POST)
+    public @ResponseBody
+    ResponseMessage eventoLanzamientoJSON(@RequestBody EventoLanzamientoData evtLanzamientoData) {
+        String code = "0";
+        String mensaje = "";
+        Jugador jugador = servicesManager.getJugadorById(evtLanzamientoData.getIdJugadorLanzador());
+        List<Integer> numeroPuntosHabilitados = Arrays.asList(EventoLanzamientoData.PUNTOS_HABILITADOS);
+        if (jugador == null) {
+            code = "-1";
+            mensaje = "Id jugador lanzador desconocido";
+        } else if (!numeroPuntosHabilitados.contains(evtLanzamientoData.getNumeroPuntos())) {
+            code = "-2";
+            mensaje = "Numero de puntos desconocido";
+        } else {
+            Evento evt = new Evento();
+            evt.setJugador(jugador);
+            evt.setFechaHora(new Date(System.currentTimeMillis()));
+            evt.setEstado(EstadoEvento.NO_ELIMINADO);
+            evt.setTipoEvento(TipoEvento.INGRESA_JUGADOR);
+            eventoManager.addEvento(evt);
+        }
+
+        int nroPuntos = evtLanzamientoData.getNumeroPuntos();
+
+        if (nroPuntos == 2 || nroPuntos == 3) {
+            code = "0";
+            mensaje = "numero de puntos:" + nroPuntos;
+        }
+        ResponseMessage responseMessage = new ResponseMessage();
+        responseMessage.setMessage(mensaje);
+        responseMessage.setCode(code);
+        return responseMessage;
+    }
 }
--- dev/Web/src/main/java/com/genfersco/sepbas/web/form/EventoLanzamientoData.java
+++ dev/Web/src/main/java/com/genfersco/sepbas/web/form/EventoLanzamientoData.java
@@ -3,17 +3,37 @@ package com.genfersco.sepbas.web.form;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonPropertyOrder;
 
-@JsonPropertyOrder({ "numeroPuntos" })
-@JsonIgnoreProperties({})
+@JsonPropertyOrder({"numeroPuntos", "idJugadorLanzador", "idJugadorAsistencia"})
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EventoLanzamientoData {
-	private Integer numeroPuntos;
 
-	public Integer getNumeroPuntos() {
-		return numeroPuntos;
-	}
+    public static final Integer[] PUNTOS_HABILITADOS = new Integer[]{1, 2, 3};
+    private Integer numeroPuntos;
+    private Integer idJugadorLanzador;
+    private Integer idJugadorAsistencia;
 
-	public void setNumeroPuntos(Integer numeroPuntos) {
-		this.numeroPuntos = numeroPuntos;
-	}
+    public Integer getNumeroPuntos() {
+        return numeroPuntos;
+    }
+
+    public void setNumeroPuntos(Integer numeroPuntos) {
+        this.numeroPuntos = numeroPuntos;
+    }
+
+    public Integer getIdJugadorLanzador() {
+        return idJugadorLanzador;
+    }
+
+    public void setIdJugadorLanzador(Integer idJugadorLanzador) {
+        this.idJugadorLanzador = idJugadorLanzador;
+    }
+
+    public Integer getIdJugadorAsistencia() {
+        return idJugadorAsistencia;
+    }
+
+    public void setIdJugadorAsistencia(Integer idJugadorAsistencia) {
+        this.idJugadorAsistencia = idJugadorAsistencia;
+    }
 
 }
