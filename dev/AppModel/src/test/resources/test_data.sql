f75cf5067f53bbda62e120bd246d18259262b4d2
Facundo Scoccia <Facundo.Scoccia@wnco.com>
2015-04-06 13:43:27 -0500

    FS - Issue #46 - Modified test data to work with events

--- dev/AppModel/src/test/resources/test_data.sql
+++ dev/AppModel/src/test/resources/test_data.sql
@@ -1,11 +1,12 @@
 --
--- Dumping data for table `arbitros`
+-- Test data for table `arbitros`
 --
 
-INSERT INTO `arbitros` (`arbitro_id`, `arbitro_nombre`, `arbitro_apellido`, `arbitro_localidad`) VALUES (4,'Leonardo',' Zalazar','Villa GG');
+INSERT INTO `arbitros` (`arbitro_id`, `arbitro_nombre`, `arbitro_apellido`, `arbitro_localidad`) 
+VALUES (1,'Leonardo',' Zalazar','Villa GG');
 
 --
--- Dumping data for table `clubes`
+-- Test data for table `clubes`
 --
 
 INSERT INTO `clubes` (`club_id`, `club_nombre`,`club_localidad`) 
@@ -16,52 +17,66 @@ VALUES
 (4,'Estudiantil','Default');
 
 --
--- Dumping data for table `jugadores`
+-- Test data for table `jugadores`
 --
-
+--
 -- Jugadores del club Libertad
+--
 INSERT INTO `jugadores` (`jugador_id`, `jugador_fec_nac`, `jugador_nombre`, `jugador_apellido`, `jugador_numero`, `jugador_actual_club_id`, `jugador_estado`) VALUES 
-(null,'2013-03-15 03:00:00','Paul','Arizin',99,1,0),
-(null,'2013-03-14 03:00:00','Charles','Barkley',22,1,0),
-(null,'2013-03-15 03:00:00','Rick','Barry',23,1,0),
-(null,'2013-03-14 03:00:00','Elgin','Baylor',33,1,0),
-(null,'2013-03-15 03:00:00','Dave','Bing',11,1,0),
-(null,'2013-03-14 03:00:00','Larry','Bird',10,1,0),
-(null,'2013-03-15 03:00:00','Wilt','Chamberlain',9,1,0),
-(null,'2013-03-14 03:00:00','Bob','Cousy',2,1,0),
-(null,'2013-03-27 03:00:00','Dave','Cowens',88,1,0),
-(null,'2013-03-27 03:00:00','Julius','Erving',89,2,0),
-(null,'2013-03-27 03:00:00','George','Gervin',12,2,0),
-(null,'2013-03-20 03:00:00','Dave','DeBusschere',14,1,0);
+(1,'2013-03-15 03:00:00','Paul','Arizin',99,1,0),
+(2,'2013-03-14 03:00:00','Charles','Barkley',22,1,0),
+(3,'2013-03-15 03:00:00','Rick','Barry',23,1,0),
+(4,'2013-03-14 03:00:00','Elgin','Baylor',33,1,0),
+(5,'2013-03-15 03:00:00','Dave','Bing',11,1,0),
+(6,'2013-03-14 03:00:00','Larry','Bird',10,1,0),
+(7,'2013-03-15 03:00:00','Wilt','Chamberlain',9,1,0),
+(8,'2013-03-14 03:00:00','Bob','Cousy',2,1,0),
+(9,'2013-03-27 03:00:00','Dave','Cowens',88,1,0),
+(10,'2013-03-27 03:00:00','Julius','Erving',89,2,0);
+
+--
 -- Jugadores del club Fisherton
+--
 INSERT INTO `jugadores` (`jugador_id`, `jugador_fec_nac`, `jugador_nombre`, `jugador_apellido`, `jugador_numero`, `jugador_actual_club_id`, `jugador_estado`) VALUES 
-(null,'2013-03-15 03:00:00','Paul','Arizin',99,2,0),
-(null,'2013-03-14 03:00:00','Charles','Barkley',22,2,0),
-(null,'2013-03-15 03:00:00','Rick','Barry',23,2,0),
-(null,'2013-03-14 03:00:00','Elgin','Baylor',33,2,0),
-(null,'2013-03-15 03:00:00','Dave','Bing',11,2,0),
-(null,'2013-03-14 03:00:00','Larry','Bird',10,2,0),
-(null,'2013-03-15 03:00:00','Wilt','Chamberlain',9,2,0),
-(null,'2013-03-14 03:00:00','Bob','Cousy',2,2,0),
-(null,'2013-03-27 03:00:00','Dave','Cowens',88,2,0),
-(null,'2013-03-27 03:00:00','Julius','Erving',89,2,0),
-(null,'2013-03-27 03:00:00','George','Gervin',12,2,0),
-(null,'2013-03-20 03:00:00','Dave','DeBusschere',14,2,0);
+(11,'2013-03-15 03:00:00','Paul','Arizin',99,2,0),
+(12,'2013-03-14 03:00:00','Charles','Barkley',22,2,0),
+(13,'2013-03-15 03:00:00','Rick','Barry',23,2,0),
+(14,'2013-03-14 03:00:00','Elgin','Baylor',33,2,0),
+(15,'2013-03-15 03:00:00','Dave','Bing',11,2,0);
 
 
 --
--- Dumping data for table `cuartos`
---
-
-INSERT INTO `cuartos` (`cuarto_id`, `cuarto_numero`, `cuarto_partido_id`) VALUES (11,1,21),(12,1,27);
-
---
--- Dumping data for table `eventos`
---
-
-INSERT INTO `eventos` (`evento_id`, `evento_generador_id`, `evento_fec_hora`, `evento_tipo_evento`, `evento_estado`, `evento_cuarto_id`, `evento_jugador_id`) VALUES (10,NULL,'2013-03-04 18:12:14',0,1,11,61),(11,NULL,'2013-03-04 18:12:14',0,1,11,61),(12,NULL,'2013-05-09 01:02:39',0,1,12,61),(13,NULL,'2013-05-09 01:02:39',0,1,12,80),(14,NULL,'2013-05-09 01:02:39',0,1,12,60);
+-- Test data for table `partidos`
+-- 
+INSERT INTO `partidos` 
+(`partido_id`, `partido_arbitro_id`, `partido_fecha`, `partido_visitante_club_id`, `partido_local_club_id`, `partido_resultado_local`, `partido_resultado_visitante`) 
+VALUES 
+(1,1,'2013-03-04',1,2,NULL,NULL),
+(2,1,'2013-03-04',2,3,NULL,NULL),
+(3,1,'2013-03-04',1,3,NULL,NULL);
 
 --
--- Dumping data for table `partidos`
+-- Test data for table `cuartos`
+-- 
+INSERT INTO `cuartos` (`cuarto_id`, `cuarto_numero`, `cuarto_partido_id`) 
+VALUES 
+(1,1,1),(2,2,1);
+
 --
-INSERT INTO `partidos` (`partido_id`, `partido_fecha`, `partido_visitante_club_id`, `partido_local_club_id`, `partido_resultado_local`, `partido_resultado_visitante`) VALUES (16,'2013-03-04',62,61,NULL,NULL),(17,'2013-03-04',62,61,NULL,NULL),(18,'2013-03-04',62,61,NULL,NULL),(19,'2013-03-04',62,61,NULL,NULL),(20,'2013-03-04',62,61,NULL,NULL),(21,'2013-03-04',62,61,NULL,NULL),(26,'2013-03-04',62,61,NULL,NULL),(27,'2013-05-08',62,61,NULL,NULL);
+-- Test data for table `eventos`
+--
+
+-- cuarto 1 partido 1 total puntos: 9
+INSERT INTO `eventos` (`evento_id`, `evento_generador_id`, `evento_fec_hora`, `evento_tipo_evento`, `evento_estado`, `evento_cuarto_id`, `evento_jugador_id`) 
+VALUES (1,NULL,'2013-03-04 18:12:14',3,1,1,1),
+(2,NULL,'2013-03-04 18:12:14',3,1,1,1),
+(3,NULL,'2013-05-09 01:02:39',3,1,1,1),
+(4,NULL,'2013-05-09 01:02:39',4,1,1,1);
+
+-- cuarto 1 partido 1 total puntos: 11
+INSERT INTO `eventos` (`evento_id`, `evento_generador_id`, `evento_fec_hora`, `evento_tipo_evento`, `evento_estado`, `evento_cuarto_id`, `evento_jugador_id`) 
+VALUES (5,NULL,'2013-03-04 18:12:14',3,1,1,12),
+(6,NULL,'2013-03-04 18:12:14',3,1,1,12),
+(7,NULL,'2013-05-09 01:02:39',3,1,1,12),
+(8,NULL,'2013-05-09 01:02:39',3,1,1,12),
+(9,NULL,'2013-05-09 01:02:39',4,1,1,12);
